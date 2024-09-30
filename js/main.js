cfc76d71fbae049bf656d7bae86be6b7fb51a022
danielsoto4 <danny_s_21@hotmail.com>
2024-09-30 19:31:34 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.body.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    const imgs = document.querySelectorAll(".img");
+    
+
+    if (!clickedElement.classList.contains("img")){
+        imgs.forEach((img)=>img.classList.remove("open"));
+        return;
+    } 
+
+    if(clickedElement.classList.contains("open")){
+        clickedElement.classList.remove("open");
+        return;
+    }
+
+    imgs.forEach((img)=>img.classList.remove("open"));
+    clickedElement.classList.add("open");
+});
